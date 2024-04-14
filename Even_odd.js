078af61a33650d06c7e4a1338119ed798ae72051
Jai Soni <jaisonikct@gmail.com>
2024-04-14 17:14:40 +0530

    Checks if the given number is even or odd

---
+++ Even_odd.js
@@ -0,0 +1,9 @@
+// Given number
+const number = prompt("Enter a number : ");
+
+// Check if the number is even or odd
+if (number % 2 === 0) {
+    console.log(`${number} is Even`);
+} else {
+    console.log(`${number} is Odd`);
+}
